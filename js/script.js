23d7725dd2d1013279c2651832ddd56cce4d985c
WashingtonYandun <washington.yandun06@gmail.com>
2022-04-24 23:48:54 -0500

    add colors to the branches

--- js/script.js
+++ js/script.js
@@ -12,18 +12,33 @@ const getRandomFloat = (min, max) => {
   return Math.random() * (max - min + 1) + min;
 };
 
-const branch = (xo, yo, lenght, branchWidth) => {
+const branch = (xo, yo, lenght, branchWidth, angle) => {
+  const colors = [
+    "#9139B4",
+    "#FE3E6D",
+    "#DA6D73",
+    "#DCB687",
+    "#42A6D8",
+    "#9b111e",
+  ];
+
   ctx.lineWidth = branchWidth;
   ctx.beginPath();
   ctx.save();
-  ctx.strokeStyle = "#000000";
-  ctx.fillStyle = "#000000";
+  ctx.strokeStyle = colors[getRandomInt(0, colors.length - 1)];
+  ctx.fillStyle = colors[getRandomInt(0, colors.length - 1)];
   ctx.translate(xo, yo);
-  ctx.rotate(0); // wanna improve this
+  ctx.rotate((angle * Math.PI) / 180); // wanna improve this
   ctx.moveTo(0, 0);
   ctx.lineTo(0, -lenght);
   ctx.stroke();
-  return;
+
+  if (lenght < 10) {
+    ctx.restore();
+    return;
+  }
+  branch(0, -lenght, lenght * 0.8, branchWidth * 0.8, angle - 15);
+  branch(0, -lenght, lenght * 0.8, branchWidth * 0.8, angle - 15);
 };
 
-branch(canvas.width / 2 + 50, canvas.height / 2 + 200, 120, 40);
+branch(canvas.width / 2 + 50, canvas.height / 2 + 200, 120, 5, 0);
